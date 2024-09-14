6aa17be4c368cde87726100411c2375f590060c0
ywonchae1 <ywonchae62@gmail.com>
2024-09-14 11:00:06 +0900

    feat: 기본 설정

---
+++ src/main/java/com/pironeer/week2_1/Week21Application.java
@@ -0,0 +1,13 @@
+package com.pironeer.week2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week21Application.class, args);
+	}
+
+}
