3ab5190053ee35616aaae7a9753977e1e54f4e28
ywonchae1 <ywonchae62@gmail.com>
2024-09-15 20:39:56 +0900

    수정 로직 도메인으로 이동

--- src/main/java/com/pironeer/week2_1/repository/domain/Topic.java
+++ src/main/java/com/pironeer/week2_1/repository/domain/Topic.java
@@ -1,5 +1,6 @@
 package com.pironeer.week2_1.repository.domain;
 
+import com.pironeer.week2_1.dto.request.TopicUpdateRequest;
 import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -24,5 +25,24 @@ public class Topic {
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+    }
+
+    public Topic update(TopicUpdateRequest request) {
+        updateTitle(request.title());
+        updateContent(request.content());
+        this.updatedAt = LocalDateTime.now();
+        return this;
+    }
+
+    private void updateTitle(String title) {
+        if (title != null && !title.isBlank()) {
+            this.title = title;
+        }
+    }
+
+    private void updateContent(String content) {
+        if (content != null && !content.isBlank()) {
+            this.content = content;
+        }
     }
 }
--- src/main/java/com/pironeer/week2_1/service/TopicService.java
+++ src/main/java/com/pironeer/week2_1/service/TopicService.java
@@ -36,23 +36,8 @@ public class TopicService {
     public TopicResponse update(TopicUpdateRequest request) {
         Topic topic = topicRepository.findById(request.id())
                 .orElseThrow(() -> new RuntimeException("TOPIC NOT FOUND"));
-        updateTitle(request.title(), topic);
-        updateContent(request.content(), topic);
-        topic.setUpdatedAt(LocalDateTime.now());
-        topicRepository.save(topic);
+        topicRepository.save(topic.update(request));
         return TopicResponse.of(topic);
-    }
-
-    private static void updateTitle(String title, Topic topic) {
-        if (title != null && !title.isBlank()) {
-            topic.setTitle(title);
-        }
-    }
-
-    private static void updateContent(String content, Topic topic) {
-        if (content != null && !content.isBlank()) {
-            topic.setContent(content);
-        }
     }
 
     public void deleteById(Long id) {
