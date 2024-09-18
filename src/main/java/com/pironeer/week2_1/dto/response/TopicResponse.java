8fcfe947a6113ba71a8cffafc8e3a50eefca922a
ywonchae1 <ywonchae62@gmail.com>
2024-09-18 19:52:40 +0900

    응답 DTO 불필요한 스웨거 제거

--- src/main/java/com/pironeer/week2_1/dto/response/TopicResponse.java
+++ src/main/java/com/pironeer/week2_1/dto/response/TopicResponse.java
@@ -10,33 +10,23 @@ import java.time.LocalDateTime;
 public record TopicResponse(
         @Schema(
                 description = "게시물 ID",
-                example = "1",
-                type = "long",
-                requiredMode = Schema.RequiredMode.REQUIRED)
+                example = "1")
         Long id,
         @Schema(
                 description = "게시물 제목",
-                example = "제목입니다",
-                type = "string",
-                requiredMode = Schema.RequiredMode.REQUIRED)
+                example = "제목입니다")
         String title,
         @Schema(
                 description = "게시물 내용",
-                example = "내용입니다",
-                type = "string",
-                requiredMode = Schema.RequiredMode.REQUIRED)
+                example = "내용입니다")
         String content,
         @Schema(
                 description = "게시물 생성 시간",
-                example = "2024-10-10 10:10:00",
-                type = "string",
-                requiredMode = Schema.RequiredMode.REQUIRED)
+                example = "2024-10-10 10:10:00")
         LocalDateTime createdAt,
         @Schema(
                 description = "게시물 수정 시간",
-                example = "2024-10-13 10:10:00",
-                type = "string",
-                requiredMode = Schema.RequiredMode.REQUIRED)
+                example = "2024-10-13 10:10:00")
         LocalDateTime updatedAt) {
     public static TopicResponse of(Topic topic) {
         return TopicResponse.builder()
